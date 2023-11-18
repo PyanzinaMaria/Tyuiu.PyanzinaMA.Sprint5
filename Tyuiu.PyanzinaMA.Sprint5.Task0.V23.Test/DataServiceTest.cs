78322f4ca08688c3640b3aea52cf6198525f7b80
Tyuiu.PyanzinaMA@outlook.com <mrpnzn@yaandex.ru>
2023-11-18 20:18:30 +0500

    Add project files.

---
+++ Tyuiu.PyanzinaMA.Sprint5.Task0.V23.Test/DataServiceTest.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.PyanzinaMA.Sprint5.Task0.V23.Lib;
+
+namespace Tyuiu.PyanzinaMA.Sprint5.Task0.V23.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidSaveToFileTextData()
+        {
+        }
+    }
+}
---
+++ Tyuiu.PyanzinaMA.Sprint5.Task0.V23.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.PyanzinaMA.Sprint5.Task0.V23.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.PyanzinaMA.Sprint5.Task0.V23.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("79b9268b-3ae9-4d8e-a299-afee196a3077")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
